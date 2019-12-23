c1bfef6d4b1a837d6cb898dc9620a1ec1caa0280
Alexy102 <59171672+Alexy102@users.noreply.github.com>
2019-12-23 21:02:28 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int symm = 0;
+            Console.WriteLine("Введите число:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i<x;i++)
+            {
+                if(x%i==0)
+                {
+                    symm += i; 
+                }
+            }
+            if(x==symm)
+            {
+                Console.WriteLine("Совершенно!");
+            }
+            else
+            {
+                Console.WriteLine("Не Совершенно!");
+            }
+        }
+    }
+}
